53a335f124d476c6e7520efe63688ef7e1850b59
Dexter Lowe <feldoh10@gmail.com>
2023-12-19 20:58:47 +0000

    Working v1

--- 1.4/Source/I_Aint_Building_That/HarmonyPatches/PatchBuildables.cs
+++ 1.4/Source/I_Aint_Building_That/HarmonyPatches/PatchBuildables.cs
@@ -12,29 +12,17 @@ namespace IAintBuildingThat.HarmonyPatches
 		[HarmonyPostfix]
 		static IEnumerable<FloatMenuOption> Postfix(IEnumerable<FloatMenuOption> opts)
 		{
-			if (Find.WindowStack.IsOpen<MainTabWindow_Architect>())
-			{
-				yield return new FloatMenuOption("Taggerung_IAintBuildingThat_HideButtonText".TranslateSimple(), () =>
-				{
-					Designator selectedDesignator = Find.DesignatorManager.SelectedDesignator ?? TrackDesignator.LatestDesignator;
-					if (selectedDesignator is not Designator_Place { PlacingDef: not null } dp) return;
-					Log.Message($"Hiding Def {dp.PlacingDef?.defName}");
-					IAintBuildingThat.settings.HiddenBuildables.Add(dp.PlacingDef?.defName);
-				});
-			}
-
 			foreach (FloatMenuOption opt in opts)
 			{
 				yield return opt;
 			}
-		}
-	}
 
-	[HarmonyPatch(typeof(PlaceWorker), "IsBuildDesignatorVisible")]
-	static class PatchOutHiddenBuildables
-	{
-		[HarmonyPostfix]
-		static bool Postfix(bool result, BuildableDef def) => result && !IAintBuildingThat.settings.HiddenBuildables.Contains(def.defName);
+			if (!Find.WindowStack.IsOpen<MainTabWindow_Architect>()) yield break;
+			Designator selectedDesignator = Find.DesignatorManager.SelectedDesignator ?? TrackDesignator.LatestDesignator;
+			if (selectedDesignator is not Designator_Place { PlacingDef: not null } dp) yield break;
+			yield return new FloatMenuOption("Taggerung_IAintBuildingThat_HideButtonText".TranslateSimple(),
+				() => IAintBuildingThat.settings.HiddenBuildables.Add(dp.PlacingDef?.defName));
+		}
 	}
 
 	[HarmonyPatch(typeof(ArchitectCategoryTab), "DoInfoBox")]
--- 1.4/Source/I_Aint_Building_That/Settings.cs
+++ 1.4/Source/I_Aint_Building_That/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -7,6 +8,8 @@ namespace IAintBuildingThat
 	public class Settings : ModSettings
 	{
 		public HashSet<string> HiddenBuildables = new();
+		private Vector2 _scrollPosition = Vector2.zero;
+		private string _searchQuery = string.Empty;
 
 		public void DoWindowContents(Rect wrect)
 		{
@@ -19,6 +22,38 @@ namespace IAintBuildingThat
 			}
 
 			options.Gap();
+
+			// Add a TextField for the search query
+			string lastSearch = _searchQuery;
+			_searchQuery = Widgets.TextField(new Rect(0, wrect.y, wrect.width, 30f), _searchQuery);
+			if (lastSearch != _searchQuery) _scrollPosition = Vector2.zero;
+
+			options.Gap();
+
+			// Filter the current list
+			List<string> filteredBuildables = HiddenBuildables.Where(buildable => _searchQuery.Length < 1 || buildable.ToLowerInvariant().Contains(_searchQuery.ToLowerInvariant()))
+				.ToList();
+
+			// Create a scrollable list
+			Rect viewRect = new(0, 0, wrect.width - 16f, filteredBuildables.Count * 35f);
+			Rect scrollRect = new(0, 80f, wrect.width, wrect.height - 80f);
+			Widgets.BeginScrollView(scrollRect, ref _scrollPosition, viewRect);
+
+			float num = 0f;
+			foreach (string buildable in filteredBuildables)
+			{
+				if (_searchQuery.Length >= 1 && !buildable.ToLowerInvariant().Contains(_searchQuery.ToLowerInvariant())) continue;
+				Rect rowRect = new(0f, num * 35f, viewRect.width, 30f);
+				if (Widgets.ButtonText(new Rect(rowRect.x + 10, rowRect.y, rowRect.width - 20f, rowRect.height), $"{"Taggerung_IAintBuildingThat_RestoreText".Translate()} {buildable}"))
+				{
+					HiddenBuildables.Remove(buildable);
+					break; // break to avoid collection modified exception
+				}
+
+				num++;
+			}
+
+			Widgets.EndScrollView();
 
 			options.End();
 		}
